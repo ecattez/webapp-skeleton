cd6b618e3c989474853a241990bb7553e8b03acb
Edouard CATTEZ <teamexcalicraft@gmail.com>
2015-03-30 18:16:34 +0200

    Exemple inscription/connexion à la base de données

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,1 +1,43 @@
 /* main.js */
+
+$(document).ready(function() {
+
+	$("#login").on("submit", function(e) { login(e) });
+	$("#subscribe").on("submit", function(e) { subscribe(e) });
+
+});
+
+function login(e) {
+	var form = function(i) { return $("#login").find("input").get(i).value; }
+	e.preventDefault(); // Annule l'action du formulaire (évite la redirection ou le raffraichissement de la page)
+	$.ajax({
+		type: "POST",
+		dataType: "json",
+		url: "/rest/usersdb/" + form(0),
+		data: { "password" : form(1) },
+		success: function(data, textStatus, jqXHR) {
+			alert("Login success");
+			localStorage["id"] = JSON.stringify(data);
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			alert("Login error");
+			localStorage["id"] = undefined;
+		}
+	});
+}
+
+function subscribe(e) {
+	var form = function(i) { return $("#subscribe").find("input").get(i).value; }
+	e.preventDefault();
+	$.ajax({
+		type: "POST",
+		url: "/rest/usersdb/",
+		data: { "login" : form(0), "password" : form(1), "firstname" : form(2), "lastname" : form(3), "birthday" : form(4), "email" : form(5) },
+		success: function(data, textStatus, jqXHR) {
+			alert("Suscribe success");
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			alert("Suscribe error");
+		}
+	});
+}
