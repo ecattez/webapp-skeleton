040a129dcfbf5cf72d5c843d449506985147e24e
Edouard CATTEZ <teamexcalicraft@gmail.com>
2015-04-17 01:05:25 +0200

    Exemple d'accès à la session dans MainResource

--- src/main/java/fr/lordrski/resources/MainResource.java
+++ src/main/java/fr/lordrski/resources/MainResource.java
@@ -1,21 +1,27 @@
 package fr.lordrski.resources;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
-import org.glassfish.jersey.process.internal.RequestScoped;
 import org.glassfish.jersey.server.mvc.Viewable;
 
 @Path("/")
-@RequestScoped
 public class MainResource {
 	
 	@GET
 	@Produces("text/html")
 	public Response index() {
 		return Response.ok(new Viewable("/index")).build();
+	}
+	
+	@GET
+	@Path("/mysession")
+	public String servlet(@Context HttpServletRequest context) {
+		return context.getSession().getId();
 	}
 
 }
